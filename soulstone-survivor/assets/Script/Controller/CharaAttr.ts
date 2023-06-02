eef5228c297056e80c7a396869a7705356dce3d9
rurutoria7 <nandokabeam@gmail.com>
2023-06-02 08:45:48 +0800

    feat: Attributes of Player, Weapon, Projectiles is now serializable.

--- soulstone-survivor/assets/Script/Controller/CharaAttr.ts
+++
@@ -1,9 +0,0 @@
-export default class CharaAttr{
-    public moveSpeed: number;
-
-    public constructor(
-            movingSpeed: number = 50
-    ){
-        this.moveSpeed = movingSpeed;
-    }
-}
