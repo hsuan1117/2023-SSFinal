8e84077099b39af539c4e6b8a3dbeac3811b13c3
Littlepants <gilbert12.tw@gmail.com>
2023-06-03 14:07:03 +0800

    Add semicolon to SearchEnemy.ts

--- wizard-survivor/assets/Script/Helper/SearchEnemy.ts
+++ wizard-survivor/assets/Script/Helper/SearchEnemy.ts
@@ -1,55 +1,44 @@
-import {nodeDistanceSqr} from "./utils";
-
 const {ccclass, property} = cc._decorator;
 
 import {ISearchTarget} from "./ISearchTarget";
+import {Deque} from '@datastructures-js/deque';
 import EnemyController from "../Controller/EnemyController";
 
 @ccclass
 export default class SearchEnemy extends cc.Component implements ISearchTarget{
 
     set searchRange(value: number) {
+        this.collider.radius = value;
         this._searchRange = value;
     }
 
     @property({type: cc.Integer, tooltip: "搜索範圍", visible: true})
-    private _searchRange: number = 200;
+    private _searchRange: number = 100;
 
-    private inRangeEnemies: {[key: string]: cc.Node} = {};
+    private collider: cc.CircleCollider = null;
 
+    private targetQueue: Deque<cc.Node> = new Deque<cc.Node>();
 
-    // LIFE-CYCLE CALLBACKS:
     onLoad() {
+        this.collider = this.node.addComponent(cc.CircleCollider);
+        this.collider.radius = this._searchRange;
     }
 
-    // PUBLIC METHODS:
-    /*
-    獲取在攻擊範圍內最近的敵人，若無回傳 null
-     */
-    public getTarget(): cc.Node {
-        const pos = this.node.convertToWorldSpaceAR(cc.Vec2.ZERO);
-        const rec = new cc.Rect(pos.x - this._searchRange, pos.y - this._searchRange, 2*this._searchRange, 2*this._searchRange);
-        const aabbRes = cc.director.getPhysicsManager().testAABB(rec);
-
-        let minDistance = Infinity;
-        let target: cc.Node = null;
-
-        for (const pc of aabbRes){
-            if (this.isTarget(pc.node)){
-                const distance = nodeDistanceSqr(this.node, pc.node);
-                if (distance < minDistance){
-                    minDistance = distance;
-                    target = pc.node;
-                }
-            }
+    onCollisionEnter(other: cc.Collider, self: cc.Collider){
+        if (this.isTarget(other.node)){
+            this.targetQueue.pushBack(other.node);
         }
-        return target;
     }
 
+    public getTarget(): cc.Node {
+        while (this.targetQueue.size() && !this.isTarget(this.targetQueue.front())){
+            this.targetQueue.popFront();
+        }
+        return this.targetQueue.size()? this.targetQueue.front(): null;
+    }
 
-    // HELPERS:
     private isTarget(target: cc.Node): boolean {
-        return target?.isValid &&
-            target.getComponent(EnemyController)?.searchable;
+        return target.getComponent(EnemyController)?.searchable &&
+                target.position.sub(this.node.position).magSqr() <= this._searchRange * this._searchRange;
     }
 }
