52bfd4d746791e434e6c851f24ab17c988fd72b2
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2023-06-10 23:35:56 +0800

    Update Deployment Script

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Wizard-Survivors
 ==================
 
 - [Project Info](https://hackmd.io/@arutoria/ryn2TPXUn)
+- [Project Demo](https://hsuan1117.github.io/2023-SSFinal/)
--- wizard-survivor/deploy.sh
+++ wizard-survivor/deploy.sh
@@ -1,3 +1,8 @@
 #!/bin/zsh
 /Applications/CocosCreator/Creator/2.4.8/CocosCreator.app/Contents/MacOS/CocosCreator --path "$(pwd)" --build "platform=web-desktop;debug=true"
+echo ""
 gh-pages -d build/web-desktop
+echo "Deployed!"
+cd ..
+wget https://github.com/hsuan1117/2023-SSFinal/archive/refs/heads/main.zip -o FinalProject.zip
+md5 FinalProject.zip
