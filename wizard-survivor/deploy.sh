a85f03a4654adc12a452de908eeea753df51154a
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2023-06-17 21:25:14 +0800

    Update deploy script according to the WaveEditor

--- wizard-survivor/deploy.sh
+++ wizard-survivor/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/zsh
 /Applications/CocosCreator/Creator/2.4.8/CocosCreator.app/Contents/MacOS/CocosCreator --path "$(pwd)" --build "platform=web-desktop;debug=true"
 echo ""
+echo "Copying 關卡編輯器..."
+cp -r ../WaveEditor build/web-desktop
 gh-pages -d build/web-desktop
 firebase deploy
 echo "Deployed!"
