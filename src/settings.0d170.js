942bc97bbb51ad8ccf5dd443e8d8744ec6416c7b
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2023-06-17 16:13:20 +0800

    Updates

---
+++ src/settings.0d170.js
@@ -0,0 +1,57 @@
+window._CCSettings = {
+    platform: "web-desktop",
+    groupList: [
+        "GameObject",
+        "Player",
+        "Camera",
+        "FixedUI",
+        "Enemy"
+    ],
+    collisionMatrix: [
+        [
+            true,
+            true,
+            false,
+            null,
+            true
+        ],
+        [
+            true,
+            true,
+            true,
+            null,
+            true
+        ],
+        [
+            false,
+            true,
+            false
+        ],
+        [
+            false,
+            false,
+            false,
+            false
+        ],
+        [
+            true,
+            true,
+            false,
+            false,
+            false
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/resources/Scene/MainScene.fire",
+    orientation: "",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "cb4f5",
+        resources: "a5121",
+        main: "319b9"
+    }
+};
