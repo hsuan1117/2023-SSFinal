4a2eaba34ca9f39436e84da9803125971eeb54d5
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2023-06-17 21:52:16 +0800

    added new script

--- wizard-survivor/deploy.sh
+++ wizard-survivor/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/zsh
+rm .firebase/*.cache
 /Applications/CocosCreator/Creator/2.4.8/CocosCreator.app/Contents/MacOS/CocosCreator --path "$(pwd)" --build "platform=web-desktop;debug=true;previewWidth=960;previewHeight=640;"
 echo ""
 echo "Copying 關卡編輯器..."
@@ -7,5 +8,5 @@ gh-pages -d build/web-desktop
 firebase deploy
 echo "Deployed!"
 cd ..
-wget https://github.com/hsuan1117/2023-SSFinal/archive/refs/heads/main.zip -o FinalProject.zip
-md5 FinalProject.zip
+wget https://github.com/hsuan1117/2023-SSFinal/archive/refs/heads/main.zip -o Final_Project_Group5.zip
+md5 Final_Project_Group5.zip
